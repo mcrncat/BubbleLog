7f7080bbddda70495a59f983824a29cca60bd4b7
BuildTools <unconfigured@null.spigotmc.org>
2025-07-13 01:41:02 +0900

    fix action 2

--- vite.config.ts
+++ vite.config.ts
@@ -1,36 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-
 import { resolve } from "node:path";
 import tailwindcss from '@tailwindcss/vite'
 import svgr from 'vite-plugin-svgr'
 
-// https://vite.dev/config/
+// リポジトリ名に合わせて明示する
+const isGitHubPages = process.env.GITHUB_PAGES === 'true'
+
 export default defineConfig({
-//   base: "/",
-  base: "/BubbleLog/",
-	root: "src",
-	plugins: [
-		react(),
-		tailwindcss(),
-		svgr()
-	],
-	publicDir: resolve(__dirname, "public"),
-	build: {
-		// dist�t�H���_�ɏo��
-		outDir: resolve(__dirname, "dist"),
-		// ���݂��Ȃ��Ƃ��̓t�H���_���쐬����
-		emptyOutDir: true,
-		copyPublicDir: true,
-		rollupOptions: {
-			// entry point������index.html�̃p�X
-			input: {
-				"": resolve(__dirname, "./src/index.html"),
-			},
-			// bundle.js�������ւ�����
-			output: {
-				entryFileNames: "assets/bundle.js",
-			},
-		},
-	},
+  base: isGitHubPages ? "/BubbleLog/" : "/",
+  root: "src",
+  plugins: [react(), tailwindcss(), svgr()],
+  publicDir: resolve(__dirname, "public"),
+  build: {
+    outDir: resolve(__dirname, "dist"),
+    emptyOutDir: true,
+    copyPublicDir: true,
+    rollupOptions: {
+      // entry を自動で探させる（index.htmlがsrcにあるなら問題なし）
+    },
+  },
 })
